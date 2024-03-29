daea10dcb61a7f2cd56647ab1567fe41cbd12f8b
naor <naor-w@apeiro-motion.com>
2024-03-29 13:53:25 +0300

    naor

--- get_vedio.py
+++ get_vedio.py
@@ -4,15 +4,12 @@ import pickle
 import struct
 
 
-class VideoServer:
+class VideoServerUDP:
     def __init__(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind(('192.168.1.193', 8080))
-        self.server_socket.listen(0)
-        self.client_socket = None
 
     def start(self):
-        self.client_socket, _ = self.server_socket.accept()
         self.receive_video()
 
     def receive_video(self):
@@ -21,14 +18,16 @@ class VideoServer:
 
         while True:
             while len(data) < payload_size:
-                data += self.client_socket.recv(4096)
+                packet, _ = self.server_socket.recvfrom(4096)
+                data += packet
 
             packed_msg_size = data[:payload_size]
             data = data[payload_size:]
             msg_size = struct.unpack("L", packed_msg_size)[0]
 
             while len(data) < msg_size:
-                data += self.client_socket.recv(4096)
+                packet, _ = self.server_socket.recvfrom(4096)
+                data += packet
 
             frame_data = data[:msg_size]
             data = data[msg_size:]
@@ -39,10 +38,9 @@ class VideoServer:
                 break
 
         cv2.destroyAllWindows()
-        self.client_socket.close()
         self.server_socket.close()
 
 
 if __name__ == "__main__":
-    video_server = VideoServer()
+    video_server = VideoServerUDP()
     video_server.start()
--- send_vedio.py
+++ send_vedio.py
@@ -3,14 +3,13 @@ import cv2
 import pickle
 import struct
 
-class VideoClient:
+class VideoClientUDP:
     def __init__(self, server_ip):
         self.server_ip = server_ip
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.vid = cv2.VideoCapture(0)
 
     def start(self):
-        self.client_socket.connect((self.server_ip, 8080))
         self.send_video()
 
     def send_video(self):
@@ -18,7 +17,7 @@ class VideoClient:
             _, frame = self.vid.read()
             data = pickle.dumps(frame)
             size = struct.pack("L", len(data))
-            self.client_socket.sendall(size + data)
+            self.client_socket.sendto(size + data, (self.server_ip, 8080))
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -27,8 +26,6 @@ class VideoClient:
         cv2.destroyAllWindows()
         self.client_socket.close()
 
-
 if __name__ == "__main__":
-        # Replace 'server_ip' with the actual IP address or hostname of the server
-    client = VideoClient('192.168.1.193')  # Example: 'localhost' or '192.168.1.100'
+    client = VideoClientUDP('192.168.1.193')  # Replace '127.0.0.1' with the server's IP address
     client.start()
